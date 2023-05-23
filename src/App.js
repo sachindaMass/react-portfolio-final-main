7f43bb05b665233c915ce127d8b80f5c89e12164
Sachinda <sachinda46@gmail.com>
2023-05-23 16:53:46 +0530

    Second

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { Routes, Route, useLocation } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import { lightTheme } from "./components/Themes";
-import { AnimatePresence } from "framer-motion";
-import GlobalStyle from "./globalStyles";
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme } from './components/Themes';
+import { AnimatePresence } from 'framer-motion';
+import GlobalStyle from './globalStyles';
+import { BrowserRouter } from 'react-router-dom';
 
 //Components
-import Main from "./components/Main";
-import AboutPage from "./components/AboutPage";
-import BlogPage from "./components/BlogPage";
-import WorkPage from "./components/WorkPage";
-import MySkillsPage from "./components/MySkillsPage";
-import SoundBar from "./subComponents/SoundBar";
+import Main from './components/Main';
+import AboutPage from './components/AboutPage';
+import BlogPage from './components/BlogPage';
+import WorkPage from './components/WorkPage';
+import MySkillsPage from './components/MySkillsPage';
+import SoundBar from './subComponents/SoundBar';
 
 function App() {
   const location = useLocation();
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
 
       <ThemeProvider theme={lightTheme}>
@@ -23,10 +24,10 @@ function App() {
 
         {/* For framer-motion animation on page change! */}
         {/* Changed prop from exitBefore to mode */}
-        <AnimatePresence mode='wait'>
+        <AnimatePresence mode="wait">
           {/* Changed Switch to Routes */}
 
-          <Routes key={location.pathname} location={location} >
+          <Routes key={location.pathname} location={location}>
             {/* Changed component to element */}
 
             <Route path="/" element={<Main />} />
@@ -44,7 +45,7 @@ you can add custom 404 component or message instead of Main component*/}
           </Routes>
         </AnimatePresence>
       </ThemeProvider>
-    </>
+    </BrowserRouter>
   );
 }
 
